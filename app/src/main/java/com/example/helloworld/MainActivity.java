9b12282a528f9c41fbbd1671363e05119b173a5e
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2020-03-02 16:42:28 +0900

    ボタン押下処理とtext表示処理追加

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.hoge).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //hogeChickEvent(v);
+                text = findViewById(R.id.textView);
+                text.append("あ");
+            }
+        });
+
+        /*
+        private void hogeChickEvent(View v)
+        {
+
+        }
+         */
     }
 }
