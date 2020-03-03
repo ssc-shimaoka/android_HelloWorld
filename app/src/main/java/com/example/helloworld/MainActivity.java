30ea48d044ff967017c5f6ba93737b55ca5f9c4a
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2020-03-03 13:08:01 +0900

    ボタン押下でlog出力するよう変更

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -21,5 +22,6 @@ public class MainActivity extends AppCompatActivity {
     {
         text = findViewById(R.id.AddTextView);
         text.append("あ");
+        Log.d("test","「あ」が追加されました");
     }
 }
